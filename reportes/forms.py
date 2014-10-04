76af12badaa718a1bf8114f540edccd762b48647
Geovanni <geovanni.berdugo@gmail.com>
2014-10-04 16:30:49 -0500

    Rediseño de interfaz con la nueva plantila que se está usando

--- reportes/forms.py
+++ reportes/forms.py
@@ -12,6 +12,11 @@ class FormularioRangoFechas(forms.Form):
     fechai = forms.DateField(label='Fecha inicial', required=True)
     fechaf = forms.DateField(label='Fecha final', required=True)
 
+    def __init__(self, *args, **kwargs):
+        super(FormularioRangoFechas, self).__init__(*args, **kwargs)
+        self.fields['fechai'].widget.attrs.update({'class' : 'form-control'})
+        self.fields['fechaf'].widget.attrs.update({'class' : 'form-control'})       
+
 MESES_CHOICES = (('1', 'Enero'), ('2', 'Febrero'), ('3', 'Marzo'), ('4', 'Abril'), ('5', 'Mayo'), ('6', 'Junio'), \
     ('7', 'Julio'), ('8', 'Agosto'), ('9', 'Septiembre'), ('10', 'Octubre'), ('11', 'Noviembre'), ('12', 'Diciembre'))
 
@@ -38,3 +43,8 @@ class FormularioReportesSinEnviar(forms.Form):
     reunion = forms.TypedChoiceField(choices = REUNION_CHOICES, coerce = int, required = True, widget = forms.RadioSelect)
     fechai = forms.DateField(label = 'Fecha inicial', required = True, widget = forms.DateInput(attrs = {'size' : 10}))
     fechaf = forms.DateField(label = 'Fecha final', required = True, widget = forms.DateInput(attrs = {'size' : 10}))
+
+    def __init__(self, *args, **kwargs):
+        super(FormularioReportesSinEnviar, self).__init__(*args, **kwargs)
+        self.fields['fechai'].widget.attrs.update({'class' : 'form-control'})
+        self.fields['fechaf'].widget.attrs.update({'class' : 'form-control'})         
