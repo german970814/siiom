6fe29e555eba02f9d32ce2d5a28618d3aa455416
German <german.alzate970814@gmail.com>
2016-02-13 11:19:07 -0500

    base cambiada

--- Iglesia/settings.py
+++ Iglesia/settings.py
@@ -55,8 +55,8 @@ DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
        'NAME': 'iglesia_prod',                      # Or path to database file if using sqlite3.
-       'USER': 'postgres',                      # Not used with sqlite3.
-       'PASSWORD': '123456',                  # Not used with sqlite3.
+       'USER': 'german1234',                      # Not used with sqlite3.
+       'PASSWORD': '1234',                  # Not used with sqlite3.
        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
    }
