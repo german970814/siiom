2746357c2ba0d2acb5110eb0469a99ee6fcad322
Tania Hernandez Narvaez <ilkalhn@gmail.com>
2016-06-16 16:34:13 -0500

    Se crea el modelo para el modulo de gestion documental

--- gestion_documental/models.py
+++ gestion_documental/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class TipoDocumento(models.Model):
+    """Modelo para guardar los tipos de documentos que manejan las distintas áreas de una iglesia."""
+
+    nombre = models.CharField(_('nombre'), max_length=100)
+    areas = models.ManyToManyField('organizacional.Area', verbose_name=_('áreas'), related_name='tipos_documento')
+
+    class Meta:
+        verbose_name = _('tipo de documento')
+        verbose_name_plural = _('tipos de documento')
+
+    def __str__(self):
+        return self.nombre.capitalize()
+
+
+class PalabraClave(models.Model):
+    """Modelo que guarda las palabras claves que se usan en los registros."""
+
+    nombre = models.CharField(_('nombre'), max_length=250)
+
+    class Meta:
+        verbose_name = _('palabra clave')
+        verbose_name_plural = _('palabras claves')
+
+    def __str__(self):
+        return self.nombre
+
+
+class Registro(models.Model):
+    """Modelo para guardar los registros de información de una iglesia."""
+
+    area = models.ForeignKey('organizacional.Area', verbose_name=_('área'), related_name='registros')
+    descripcion = models.TextField(_('descripción'))
+    palabras_claves = models.ManyToManyField(
+        PalabraClave,
+        verbose_name=_('palabras claves'),
+        related_name='registros',
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = _('registro')
+        verbose_name_plural = _('registros')
+
+
+class Documento(models.Model):
+    """Modelo que guarda los archivos relacionados a un registro."""
+
+    def ruta_archivo(self, filename):
+        registro = self.registro
+        return 'gestion_documental/area_{}/registro_{}/{}'.format(registro.area.id, registro.id, self.filename)
+
+    registro = models.ForeignKey(Registro, verbose_name=_('registro'), related_name='documentos')
+    tipo_documento = models.ForeignKey(TipoDocumento, verbose_name=_('tipo de documento'), related_name='documentos')
+    archivo = models.FileField(_('archivo'), upload_to=ruta_archivo)
+
+    class Meta:
+        verbose_name = _('documento')
+        verbose_name_plural = _('documentos')
--- organizacional/models.py
+++ organizacional/models.py
@@ -1,1 +1,44 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+
+class Departamento(models.Model):
+    """Modelo para guardar los departamentos de una iglesia."""
+
+    nombre = models.CharField(_('nombre'), max_length=100)
+
+    class Meta:
+        verbose_name = _('departamento')
+        verbose_name_plural = _('departamentos')
+
+    def __str__(self):
+        return self.nombre.title()
+
+
+class Area(models.Model):
+    """Modelo para guardar las áreas de una iglesia."""
+
+    nombre = models.CharField(_('nombre'), max_length=100)
+    departamento = models.ForeignKey(Departamento, verbose_name=_('departamento'), related_name='areas')
+
+    class Meta:
+        verbose_name = _('área')
+        verbose_name_plural = _('áreas')
+
+    def __str__(self):
+        return self.nombre.title()
+
+
+class Empleado(models.Model):
+    """Modelo que guarda las personas que trabajan actualmente para una iglesia."""
+
+    usuario = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name=_('usuario'))
+    areas = models.ManyToManyField(Area, verbose_name=_('áreas'), related_name='empleados')
+
+    class Meta:
+        verbose_name = _('empleado')
+        verbose_name_plural = _('empleados')
+
+    def __str__(self):
+        return self.usuario
