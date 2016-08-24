8f2104a5da9cea7d0d641ebd6263f0a5b417fdad
Tania Melissa Hernandez Narvaez <ilkalhn@gmail.com>
2016-08-24 16:02:34 -0500

    Se inicia la prueba obtener el árbol de grupos

--- grupos/tests/factories.py
+++ grupos/tests/factories.py
@@ -5,17 +5,29 @@ from grupos import models
 from miembros.tests.factories import MiembroFactory
 
 
+class RedFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Red
+        django_get_or_create = ('nombre',)
+
+    nombre = 'jovenes'
+
+
 class GrupoFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Grupo
 
     fechaApertura = factory.LazyFunction(datetime.datetime.now)
     lider1 = factory.SubFactory(MiembroFactory)
+    red = factory.SubFactory(RedFactory)
     horaDiscipulado = factory.Faker('time')
     horaGAR = factory.Faker('time')
     barrio_id = 1
-    red_id = 1
 
 
 class GrupoRaizFactory(GrupoFactory):
-    red_id = None
+    red = None
+
+
+class GrupoHijoFactory(GrupoFactory):
+    red = factory.lazy_attribute(lambda o: o.parent.red)
--- grupos/tests/test_models.py
+++ grupos/tests/test_models.py
@@ -1,22 +1,30 @@
 from django.test import TestCase
-from .factories import GrupoFactory, GrupoRaizFactory
+from .factories import GrupoFactory, GrupoRaizFactory, GrupoHijoFactory
+from grupos.models import Grupo
 
 
 class GrupoModelTest(TestCase):
 
-    def crear_arbol(self):
+    def setUp(self):
         padre = GrupoRaizFactory(id=1)
-        cabeza_red1 = GrupoFactory(id=2, parent=padre)
+        cabeza_red1 = GrupoFactory(id=2, parent=padre, red__nombre='matrimonio')
         cabeza_red2 = GrupoFactory(id=3, parent=padre)
-        cabeza_red3 = GrupoFactory(id=4, parent=padre)
+        cabeza_red3 = GrupoFactory(id=4, parent=padre, red__nombre='adultos')
 
-        hijo1_cb2 = GrupoFactory(id=5, parent=cabeza_red2)
-        hijo2_cb2 = GrupoFactory(id=8, parent=cabeza_red2)
-        hijo11_cb2 = GrupoFactory(id=6, parent=hijo1_cb2)
+        hijo1_cb2 = GrupoHijoFactory(id=5, parent=cabeza_red2)
+        hijo2_cb2 = GrupoHijoFactory(id=8, parent=cabeza_red2)
+        hijo11_cb2 = GrupoHijoFactory(id=6, parent=hijo1_cb2)
 
-        hijo1_cb3 = GrupoFactory(id=7, parent=cabeza_red3)
+        hijo1_cb3 = GrupoHijoFactory(id=7, parent=cabeza_red3)
 
-    def test_obtener_arbol(self):
-        """"""
+        self.lista_arbol_completo = [
+            padre, [cabeza_red1, cabeza_red2, [hijo1_cb2, [hijo11_cb2], hijo2_cb2], cabeza_red3, [hijo1_cb3]]
+        ]
 
-        self.crear_arbol()
+    def test_obtener_arbol_completo(self):
+        """Lista obtenida sea igual a la lista del arbol completo """
+
+        lista_obtenida = Grupo.obtener_arbol()
+
+    def test_obtener_arbol_padre_especifico(self):
+        pass
