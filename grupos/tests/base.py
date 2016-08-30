d248a5b902fedb76f0d4334461c3c01e8938c270
Tania Melissa Hernandez Narvaez <ilkalhn@gmail.com>
2016-08-30 12:54:16 -0500

    Se crea el árbol en un test base

---
+++ grupos/tests/base.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from .factories import GrupoFactory, GrupoRaizFactory, GrupoHijoFactory
+
+
+class BaseTest(TestCase):
+    """
+    Clase base para las pruebas de la app grupos.
+    """
+
+    def setUp(self):
+        padre = GrupoRaizFactory(id=1)
+        cabeza_red1 = GrupoFactory(id=2, parent=padre, red__nombre='matrimonio')
+        cabeza_red2 = GrupoFactory(id=3, parent=padre)
+        cabeza_red3 = GrupoFactory(id=4, parent=padre, red__nombre='adultos')
+
+        hijo1_cb2 = GrupoHijoFactory(id=5, parent=cabeza_red2)
+        hijo2_cb2 = GrupoHijoFactory(id=8, parent=cabeza_red2)
+        hijo11_cb2 = GrupoHijoFactory(id=6, parent=hijo1_cb2)
+
+        hijo1_cb3 = GrupoHijoFactory(id=7, parent=cabeza_red3)
+
+        self.lista_arbol_completo = [
+            padre, [cabeza_red1, cabeza_red2, [hijo1_cb2, [hijo11_cb2], hijo2_cb2], cabeza_red3, [hijo1_cb3]]
+        ]
+
+        self.lista_arbol_cb2 = [
+            cabeza_red2, [hijo1_cb2, [hijo11_cb2], hijo2_cb2]
+        ]
--- grupos/tests/test_models.py
+++ grupos/tests/test_models.py
@@ -1,29 +1,12 @@
 from django.test import TestCase
-from .factories import GrupoFactory, GrupoRaizFactory, GrupoHijoFactory
+from .base import BaseTest
 from grupos.models import Grupo
 
 
-class GrupoModelTest(TestCase):
-
-    def setUp(self):
-        padre = GrupoRaizFactory(id=1)
-        cabeza_red1 = GrupoFactory(id=2, parent=padre, red__nombre='matrimonio')
-        cabeza_red2 = GrupoFactory(id=3, parent=padre)
-        cabeza_red3 = GrupoFactory(id=4, parent=padre, red__nombre='adultos')
-
-        hijo1_cb2 = GrupoHijoFactory(id=5, parent=cabeza_red2)
-        hijo2_cb2 = GrupoHijoFactory(id=8, parent=cabeza_red2)
-        hijo11_cb2 = GrupoHijoFactory(id=6, parent=hijo1_cb2)
-
-        hijo1_cb3 = GrupoHijoFactory(id=7, parent=cabeza_red3)
-
-        self.lista_arbol_completo = [
-            padre, [cabeza_red1, cabeza_red2, [hijo1_cb2, [hijo11_cb2], hijo2_cb2], cabeza_red3, [hijo1_cb3]]
-        ]
-
-        self.lista_arbol_cb2 = [
-            cabeza_red2, [hijo1_cb2, [hijo11_cb2], hijo2_cb2]
-        ]
+class GrupoModelTest(BaseTest):
+    """
+    Prueba el modelo Grupo.
+    """
 
     def test_obtener_raiz_arbol(self):
         """Grupo obtenido sea la raiz del arbol."""
