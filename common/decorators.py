979111ff4248868d46977af5a6a05991129ed7cf
Tania Melissa Hernandez Narvaez <ilkalhn@gmail.com>
2016-09-02 22:46:43 -0500

    Se agrega decorador para chequear si un usuario tiene cualquiera de los permisos indicados

---
+++ common/decorators.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.decorators import user_passes_test
+from django.core.exceptions import PermissionDenied
+
+def permisos_requeridos(*permisos):
+    """
+    Decorador para vistas que chequea si el usuario tiene algún permiso habilitado. Si no tiene ningún permiso se
+    levanta la excepción PermissionDenied.
+    """
+
+    def chequear_permisos(user):
+        for permiso in permisos:
+            if user.has_perm(permiso):
+                return True
+
+        raise PermissionDenied
+        return False
+
+    return user_passes_test(chequear_permisos)
