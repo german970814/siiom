fe8d43200f12acdcc5cbfb356e0108c4ea859662
Tania Melissa Hernandez Narvaez <ilkalhn@gmail.com>
2016-09-13 12:12:47 -0500

    Organizan los imports

--- miembros/models.py
+++ miembros/models.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
-from django.db import models
-from django.contrib.auth.models import User
 import datetime
+from django.db import models
 from django.db.models import Q
+from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from .managers import MiembroManager
 
