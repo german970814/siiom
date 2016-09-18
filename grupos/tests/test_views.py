67a77cabdf7d14203b20a980c48bbc1738c439d6
Tania Melissa Hernandez Narvaez <ilkalhn@gmail.com>
2016-09-18 15:41:11 -0500

    PEP8 cleaned up.

--- grupos/tests/test_views.py
+++ grupos/tests/test_views.py
@@ -23,27 +23,35 @@ class OrganigramaGruposViewTest(GruposBaseTest):
         super(OrganigramaGruposViewTest, self).setUp()
 
     def ingresar_pagina(self, login=True):
-        """Ingresa a la página del organigrama de grupos y retorna el reponse. Por defecto loguea al usuario."""
+        """
+        Ingresa a la página del organigrama de grupos y retorna el reponse. Por defecto loguea al usuario.
+        """
 
         if login:
             self.login_usuario()
         return self.client.get(self.URL)
 
     def test_usuario_no_logueado_redireccionado_login(self):
-        """Prueba que un usuario no logueado sea redireccionado al login."""
+        """
+        Prueba que un usuario no logueado sea redireccionado al login.
+        """
 
         response = self.ingresar_pagina(login=False)
         self.assertRedirects(response, '{0}?next={1}'.format(reverse('inicio'), self.URL))
 
     def test_usuario_logueado_no_lider_ni_admin_redireccionado_sin_permisos(self):
-        """Prueba que un usuario logueado que no sea administrador ni líder sea redireccionado a página que indique que
-        no tiene permisos."""
+        """
+        Prueba que un usuario logueado que no sea administrador ni líder sea redireccionado a página que indique que
+        no tiene permisos.
+        """
 
         response = self.ingresar_pagina()
         self.assertEqual(response.status_code, 403)
 
     def test_admin_vea_arbol_grupos_completo(self):
-        """Prueba que un administrador pueda ver el arbol completo de la iglesia."""
+        """
+        Prueba que un administrador pueda ver el arbol completo de la iglesia.
+        """
 
         self.usuario.user_permissions.add(Permission.objects.get(codename='es_administrador'))
         response = self.ingresar_pagina()
@@ -53,7 +61,9 @@ class OrganigramaGruposViewTest(GruposBaseTest):
         self.assertListEqual(response.context['arbol'], self.lista_arbol_completo)
 
     def test_lider_vea_arbol_desde_grupo_lidera(self):
-        """Prueba que un líder solo pueda ver el arbol desde su grupo hacia abajo."""
+        """
+        Prueba que un líder solo pueda ver el arbol desde su grupo hacia abajo.
+        """
 
         self.usuario.user_permissions.add(Permission.objects.get(codename='es_lider'))
         miembro = Grupo.objects.get(id=3).lider1
