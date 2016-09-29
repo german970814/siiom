a0787ef57ed7cb9959f16afc604550c147a00459
Tania Melissa Hernandez Narvaez <ilkalhn@gmail.com>
2016-09-29 11:50:29 -0500

    Se agregan formularios bases con custom error list

---
+++ common/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.forms.utils import ErrorList
+from django.utils.encoding import force_text
+from django.utils.html import format_html_join
+
+
+class CustomErrorList(ErrorList):
+
+    def __str__(self):
+        return self.as_material()
+
+    def as_material(self):
+        if not self.data:
+            return ''
+
+        return format_html_join('', '<small class="help-block">{}</small>', ((force_text(e),) for e in self))
+
+
+class CustomModelForm(forms.ModelForm):
+    """
+    Formulario base para ModelForm.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(CustomModelForm, self).__init__(error_class=CustomErrorList, *args, **kwargs)
+
+
+class CustomForm(forms.Form):
+    """
+    Formulario base para Form.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(CustomForm, self).__init__(error_class=CustomErrorList, *args, **kwargs)
