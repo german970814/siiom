1bd1e50ee336550aaf6c3d9c53469ac6833fdeda
Tania Melissa Hernandez Narvaez <ilkalhn@gmail.com>
2016-10-31 12:08:24 -0500

    Se termina la revision de la app consolidation

--- consolidacion/urls.py
+++ consolidacion/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import include, patterns, url
-from .views import *
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    url(r'^asignar_grupo_visitas/$', asignar_grupo_visitas, name="asignar_grupo_visitas"),
-    url(r'^visitas/nueva/$', CrearVisita.as_view(), name="crear_visita"),
-    url(r'^visitas/editar/(?P<pk>\d+)/$', EditarVisita.as_view(), name="editar_visita"),
-    url(r'^/api/visitas/asignar/$', asignar_grupo_visitas_ajax, name="asignar_grupo_visitas_ajax"),
+    url(r'^asignar_grupo_visitas/$', views.asignar_grupo_visitas, name="asignar_grupo_visitas"),  # revisada
+    url(r'^visitas/nueva/$', views.CrearVisita.as_view(), name="crear_visita"),  # revisada
+    url(r'^visitas/editar/(?P<pk>\d+)/$', views.EditarVisita.as_view(), name="editar_visita"),  # revisada
+    url(r'^/api/visitas/asignar/$', views.asignar_grupo_visitas_ajax, name="asignar_grupo_visitas_ajax"),  # revisada
 ]
