f7bbff55c6217fd2185d99e8f14f02c64c051745
Tania Melissa Hernandez Narvaez <ilkalhn@gmail.com>
2016-11-21 16:42:25 -0500

    PEP8 cleaned up

--- common/decorators.py
+++ common/decorators.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.core.exceptions import PermissionDenied
 
+
 def permisos_requeridos(*permisos):
     """
     Decorador para vistas que chequea si el usuario tiene algún permiso habilitado. Si no tiene ningún permiso se
