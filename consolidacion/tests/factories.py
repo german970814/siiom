8e6d6bc78ff86f803a94e417d7c8ef068497766b
Tania Melissa Hernandez Narvaez <ilkalhn@gmail.com>
2017-01-18 13:06:52 -0500

    Se crea factories para los modelos encuentros y encontristas

--- consolidacion/tests/factories.py
+++ consolidacion/tests/factories.py
@@ -6,7 +6,7 @@ class VisitaFactory(factory.DjangoModelFactory):
     class Meta:
         model = 'consolidacion.Visita'
 
+    genero = 'F'
+    telefono = 3542345
     primer_nombre = factory.Faker('first_name', locale='es')
     primer_apellido = factory.Faker('last_name', locale='es')
-    telefono = 3542345
-    genero = 'F'
