827dffacde614a7f89f86800a9913d535b804af8
Tania Melissa Hernandez Narvaez <ilkalhn@gmail.com>
2017-03-31 09:40:46 -0500

    Se borra viejo fabfile y se modifica logging para hacerlo por tenant

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,141 +1,180 @@
-from fabric.contrib.files import append, exists, sed
-from fabric.api import env, local, run, hosts
 import random
+from contextlib import contextmanager
+from fabric.contrib.files import append, sed
+from fabric.api import env, task, run, sudo, cd, prefix, local
 
-REPO_URL = 'https://taniamhn@bitbucket.org/ingeniarte/iglesia.git'
+REPO_URL = 'git@bitbucket.org:ingeniarte/siiom.git'
+# psql -U siiom -h localhost siiom < ~/Desktop/staging_tenant.sql
+# rsync -nrv --exclude=.DS_Store . ingeniarte@staging.siiom.net:/home/ingeniarte/sites/ingeniarte.siiom.net/media/cdr.siiom.net/
+# return 301 $scheme://tenant.staging.siiom.net$request_uri;
 
-env.hosts = ['iglesia.webfactional.com', 'panama.iglesia.webfactional.com',
-             'gng.iglesia.webfactional.com', 'cdo.iglesia.webfactional.com']
-env.user = 'iglesia'
-
-enviroments = {
-    'siiom.conial.net': {
-        'name': 'siiom',
-        'db': 'siiom',
-        'user': 'siiom',
-        'pass': '123456',
-        'source': 'develop'
+ENVIROMENT_SETTINGS = {
+    'production': {
+        'site': 'ingeniarte.siiom.net',
+        'branch': 'feature/tenants',
+        'allowed_host': '.siiom.net',
+        'db': {
+            'name': 'siiom',
+            'pass': 'dbsiiom2017',
+            'user': 'siiom'
+        }
     },
-
-    'iglesia.webfactional.com': {
-        'name': 'cdr',
-        'db': 'iglesia_prod',
-        'user': 'iglesia',
-        'pass': 'ecbddac9',
-        'source': 'master'
-    },
-
-    'panama.iglesia.webfactional.com': {
-        'name': 'vps',
-        'db': 'panama_prod',
-        'user': 'iglesia',
-        'pass': 'ecbddac9',
-        'source': 'master'
-    },
-
-    'gng.iglesia.webfactional.com': {
-        'name': 'gng',
-        'db': 'gng',
-        'user': 'iglesia',
-        'pass': 'ecbddac9',
-        'source': 'master'
-    },
-
-    'cdo.iglesia.webfactional.com': {
-        'name': 'cdo',
-        'db': 'cdo',
-        'user': 'iglesia',
-        'pass': 'ecbddac9',
-        'source': 'master'
+    'staging': {
+        'site': 'staging.siiom.net',
+        'branch': 'feature/tenants',
+        'allowed_host': '.staging.siiom.net',
+        'db': {
+            'name': 'siiom_staging',
+            'pass': 'dbsiiom2017',
+            'user': 'siiom'
+        }
     }
 }
 
 
-@hosts('conial@siiom.conial.net')
-def staging():
-    deploy()
+def site_dir():
+    global SITE_FOLDER, PROJECT_ROOT
+    env.site = env.settings['site']
+    SITE_FOLDER = '/home/{user}/sites/{site}'.format(user=env.user, site=env.site)
+    PROJECT_ROOT = '{}/src'.format(SITE_FOLDER)
 
 
-def deploy():
-    site_folder = '/home/%s/webapps/siiom/%s' % (env.user, env.host)
-    source_folder = site_folder + '/source'
-    _create_directory_structure_if_necessary(site_folder)
-    _get_latest_source(source_folder)
-    _update_settings(source_folder, env.host)
-    _update_virtualenv(env.user, env.host, source_folder)
-    _update_static_files(source_folder, env.user, env.host)
-    _update_database_info(source_folder, env.host)
-    _update_database(source_folder, env.user, env.host)
-    _restart_gunicorn_server(env.user, env.host)
+@contextmanager
+def virtualenv():
+    with prefix('workon {}'.format(env.site)):
+        yield
 
 
-def _create_directory_structure_if_necessary(site_folder):
-    for subfolder in ('static', 'source', 'media'):
-        run('mkdir -p %s/%s' % (site_folder, subfolder))
+def create_secret_key():
+    secret_key_file = PROJECT_ROOT + '/siiom/secret_key.py'
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
+    append(secret_key_file, "SECRET_KEY = '{}'".format(key))
 
 
-def _get_latest_source(source_folder):
-    if exists(source_folder + '/.git'):
-        run('cd %s && git fetch' % (source_folder,))
-    else:
-        run('git clone %s %s' % (REPO_URL, source_folder))
-
-    source = enviroments[env.host]['source']
-
-    current_commit = local("git log origin/" + source + " -n 1 --format=%H", capture=True)
-    run('cd %s && git reset --hard %s' % (source_folder, current_commit))
-
-
-def _update_settings(source_folder, site_name):
-    settings_path = source_folder + '/Iglesia/settings.py'
+def update_settings():
+    settings_path = PROJECT_ROOT + '/siiom/settings.py'
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(settings_path,
         'ALLOWED_HOSTS =.+$',
-        'ALLOWED_HOSTS = ["%s", "siiom.net", "demo.siiom.net"]' % (site_name,)
+        'ALLOWED_HOSTS = ["{}"]'.format(env.settings['allowed_host'])
         )
-
-    secret_key_file = source_folder + '/Iglesia/secret_key.py'
-    if not exists(secret_key_file):
-        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-        key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
-        append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
 
     append(settings_path, '\nfrom .secret_key import SECRET_KEY')
 
 
-def _update_virtualenv(user, site_name, source_folder):
-    virtualenv_folder = '/home/%s/.envs/%s' % (user, site_name)
-    if not exists(virtualenv_folder + '/bin/pip'):
-        run('virtualenv --python=python3 %s' % (virtualenv_folder,))
-        run('%s/bin/pip install newrelic' % (virtualenv_folder,))
+def update_database():
+    database_path = PROJECT_ROOT + '/siiom/database.py'
+    db_settings = env.settings['db']
 
-    run('%s/bin/pip install -r %s/requirements/production.txt' % (
-        virtualenv_folder, source_folder
-    ))
+    sed(database_path, 'NAME = ""', 'NAME = "%s"' % (db_settings['name']))
+    sed(database_path, 'USER = ""', 'USER = "%s"' % (db_settings['user']))
+    sed(database_path, 'PASSWORD = ""', 'PASSWORD = "%s"' % (db_settings['pass']))
 
 
-def _update_static_files(source_folder, user, site_name):
-    run('cd %s && /home/%s/.envs/%s/bin/python3 manage.py collectstatic --noinput' % (
-        source_folder, user, site_name
-    ))
+def update_source():
+    commit = local('git log origin2/{} -n 1 --format=%H'.format(env.settings['branch']), capture=True)
+
+    with virtualenv():
+        run('git fetch')
+        run('git reset --hard {}'.format(commit))
 
 
-def _update_database_info(source_folder, host):
-    database_path = source_folder + '/Iglesia/database.py'
+def config_gunicorn():
+    file_ = 'gunicorn_start'
+    with cd('{}/bin'.format(SITE_FOLDER)):
+        sudo('cp {}/deploy_tools/gunicorn.template.conf {}'.format(PROJECT_ROOT, file_))
+        sed(file_, 'PROJECT_ROOT', PROJECT_ROOT)
+        sed(file_, 'SITE_FOLDER', SITE_FOLDER)
+        sed(file_, 'SITENAME', env.site)
+        sed(file_, 'USER', env.user)
 
-    valores = enviroments[host]
-    sed(database_path, 'NAME = ""', 'NAME = "%s"' % (valores['db']))
-    sed(database_path, 'USER = ""', 'USER = "%s"' % (valores['user']))
-    sed(database_path, 'PASSWORD = ""', 'PASSWORD = "%s"' % (valores['pass']))
-
-
-def _update_database(source_folder, user, site_name):
-    run('cd %s && /home/%s/.envs/%s/bin/python3 manage.py migrate --noinput' % (
-        source_folder, user, site_name
-    ))
+        run('chmod u+x {}'.format(file_))
 
 
-def _restart_gunicorn_server(user, host):
-    name = enviroments[host]['name']
-    run('/home/%s/bin/supervisorctl -c /home/%s/etc/supervisord.conf restart %s' % (user, user, name))
-    run('/home/%s/bin/supervisorctl -c /home/%s/etc/supervisord.conf status %s' % (user, user, name))
+def config_services():
+    CONFIG_FILES = {
+        'supervisor': 'conf.d/{}.conf'.format(env.site),
+        'nginx': 'sites-available/{}.conf'.format(env.site)
+    }
+
+    for service in ('nginx', 'supervisor'):
+        with cd('/etc/{}/'.format(service)):
+            sudo('cp {}/deploy_tools/{}.template.conf {}'.format(PROJECT_ROOT, service, CONFIG_FILES[service]))
+            sed(CONFIG_FILES[service], 'HOSTNAME', env.settings['allowed_host'], use_sudo=True, shell=True)
+            sed(CONFIG_FILES[service], 'PROJECT_ROOT', PROJECT_ROOT, use_sudo=True, shell=True)
+            sed(CONFIG_FILES[service], 'SITE_FOLDER', SITE_FOLDER, use_sudo=True, shell=True)
+            sed(CONFIG_FILES[service], 'SITENAME', env.site, use_sudo=True, shell=True)
+            sed(CONFIG_FILES[service], 'USER', env.user, use_sudo=True, shell=True)
+
+    sudo('ln -s {nginx}/{} {nginx}/sites-enabled'.format(CONFIG_FILES['nginx'], env.site, nginx='/etc/nginx'))
+    config_gunicorn()
+
+
+@task
+def staging():
+    """Setting staging enviroment."""
+
+    env.user = 'ingeniarte'
+    env.hosts = ['staging.siiom.net']
+    env.settings = ENVIROMENT_SETTINGS['staging']
+
+    site_dir()
+
+
+@task
+def production():
+    """Setting production enviroment."""
+
+    env.user = 'ingeniarte'
+    env.hosts = ['ingeniarte.siiom.net']
+    env.settings = ENVIROMENT_SETTINGS['production']
+
+    site_dir()
+
+
+@task
+def provision():
+    """Provision a new site on an already provision server."""
+
+    for subfolder in ('static', 'media', 'src', 'bin', 'tmp/sockets', 'tmp/logs'):
+        run("mkdir -p {}/{}".format(SITE_FOLDER, subfolder))
+
+    run('git clone {} {}'.format(REPO_URL, PROJECT_ROOT))
+
+    #  Create virtualenv
+    run('mkvirtualenv {}'.format(env.site))
+    with cd(PROJECT_ROOT):
+        with virtualenv():
+            run('setvirtualenvproject')
+
+    update_source()
+    create_secret_key()
+    update_database()
+    update_settings()
+    with virtualenv():
+        run('pip install -r requirements/production.txt')
+        run('./manage.py migrate_schemas --shared')
+        run('./manage.py collectstatic --noinput')
+
+    config_services()
+
+    sudo('systemctl reload nginx')
+    sudo('supervisorctl reread')
+    sudo('supervisorctl update')
+
+
+@task
+def deploy():
+    """Deploy new changes to the server"""
+
+    update_source()
+    update_settings()
+    update_database()
+    with virtualenv():
+        run('pip install -r requirements/production.txt')
+        run('./manage.py migrate_schemas')
+        run('./manage.py collectstatic --noinput')
+
+    sudo('supervisorctl restart {}'.format(env.site))
+    sudo('supervisorctl status {}'.format(env.site))
