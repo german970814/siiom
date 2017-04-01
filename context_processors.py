6c4bd281b915b3b4720b278f36dc0fedc98127f2
Tania Melissa Hernandez Narvaez <ilkalhn@gmail.com>
2017-04-01 11:18:24 -0500

    Se simplifica context_processor

--- context_processors.py
+++ context_processors.py
@@ -1,6 +1,4 @@
 from contextlib import suppress
-from django.contrib.sites.models import Site
-from miembros.models import Miembro
 from grupos.models import HistorialEstado
 
 
@@ -15,16 +13,15 @@ def siiom_context_processor(request):
         'GRUPO_SUSPENDIDO': HistorialEstado.SUSPENDIDO
     }
 
-    if hasattr(request, 'user'):
-        if request.user.is_authenticated():
-            with suppress(Exception):
-                miembro = Miembro.objects.get(usuario=request.user)
-                draw_mapa = False
+    if hasattr(request, 'miembro'):
+        with suppress(Exception):
+            miembro = request.miembro
+            draw_mapa = False
 
-                if miembro.grupo_lidera is not None:
-                    if miembro.grupo_lidera.get_position() is None:
-                        draw_mapa = True
+            if miembro.grupo_lidera is not None:
+                if miembro.grupo_lidera.get_position() is None:
+                    draw_mapa = True
 
-                data['draw_mapa'] = draw_mapa
+            data['draw_mapa'] = draw_mapa
 
     return data
