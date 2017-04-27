ce6e58f22a50d93e9a5223d10b2528a51dbcce54
Tania Melissa Henandez Narvaez <ilkalhn@gmail.com>
2017-04-27 12:52:29 -0500

    Se agrega settings por entorno

---
+++ siiom/settings/dev.py
@@ -0,0 +1,28 @@
+from .base import *
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.security.SecurityMiddleware',
+    'tenant_schemas.middleware.TenantMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'waffle.middleware.WaffleMiddleware',
+    'miembros.middleware.MiembroMiddleWare',
+    'organizacional.middleware.EmpleadoMiddleWare',
+)
+
+#  Apps
+SHARED_APPS += [
+    'django_extensions',
+    'debug_toolbar',
+]
+
+INSTALLED_APPS = SHARED_APPS + list(set(TENANT_APPS) - set(SHARED_APPS))
+
+#  Debug toolbar
+INTERNAL_IPS = ['127.0.0.1']
