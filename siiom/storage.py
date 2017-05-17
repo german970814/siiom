52e7717e8fed1db6fc8de20cd4b1b56233decc19
Tania Melissa Henandez Narvaez <ilkalhn@gmail.com>
2017-05-17 17:40:59 -0500

    PEP8 cleaned up

--- siiom/storage.py
+++ siiom/storage.py
@@ -9,10 +9,13 @@ from tenant_schemas.storage import TenantFileSystemStorage
 
 
 def url(self, name):
+    """Permite guardar según tenants."""
+
     if self.base_url is None:
         raise ValueError("This file is not accessible via a URL.")
 
     base_url = "{0}{1}/".format(self.base_url, connection.tenant.domain_url)
     return urljoin(base_url, filepath_to_uri(name))
+
 
 TenantFileSystemStorage.url = url
