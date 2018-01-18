e7506e615c726dfdee694df3159a7d008dc6c4d7
Tania Melissa Hernandez Narvaez <taniamhn@gmail.com>
2018-01-18 11:05:11 -0500

    se optimizan consultas en vista de grupos en admin

--- grupos/admin.py
+++ grupos/admin.py
@@ -4,25 +4,34 @@ Created on Apr 1, 2011
 @author: Migue
 '''
 # Django imports
+from django import forms
 from django.contrib import admin
-
-# Locale imports
 from .models import Red, Grupo, ReunionGAR, ReunionDiscipulado, Predica
 
 
+@admin.register(Grupo)
 class GrupoAdmin(admin.ModelAdmin):
     search_fields = ['nombre', 'direccion', 'lideres__nombre', 'lideres__cedula']
     list_display = ('__str__', 'get_estado_display', 'red', )
     list_filter = ('red', 'historiales__estado', )
     list_select_related = ('red', )
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related('lideres', 'historiales')
 
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "parent":
+            kwargs["queryset"] = Grupo.objects.prefetch_related('historiales', 'lideres').all()
+        
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+@admin.register(ReunionGAR)
 class ReunionGARAdmin(admin.ModelAdmin):
     search_fields = ['grupo__nombre']
 
 
 admin.site.register(Red)
-admin.site.register(Grupo, GrupoAdmin)
-admin.site.register(ReunionGAR, ReunionGARAdmin)
 admin.site.register(ReunionDiscipulado)
 admin.site.register(Predica)
